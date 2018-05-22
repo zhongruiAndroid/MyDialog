e4b9d271a58161e96df6230d6222e04c3000dafd
2380253499@qq.com <2380253499@qq.com>
2018-05-22 17:04:39 +0800

    dialog工具类

---
+++ app/src/main/java/com/test/mydialog/view/CircleProgress.java
@@ -0,0 +1,27 @@
+package com.test.mydialog.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2018/5/22.
+ */
+
+public class CircleProgress extends View {
+
+
+    public CircleProgress(Context context) {
+        super(context);
+    }
+
+    public CircleProgress(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleProgress(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
