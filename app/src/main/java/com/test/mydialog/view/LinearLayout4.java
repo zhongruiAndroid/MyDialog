de01298bdcb3e46eff6a29a437f8688df96345b8
2380253499@qq.com <2380253499@qq.com>
2018-06-29 17:18:03 +0800

    项目文件

--- app/src/main/java/com/test/mydialog/view/LinearLayout4.java
+++ app/src/main/java/com/test/mydialog/view/LinearLayout4.java
@@ -1,24 +1,24 @@
 package com.test.mydialog.view;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
-import android.graphics.PaintFlagsDrawFilter;
 import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.RadialGradient;
 import android.graphics.RectF;
 import android.graphics.Region;
+import android.graphics.Shader;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.View;
+import android.view.MotionEvent;
 import android.widget.LinearLayout;
 
 import com.test.mydialog.BuildConfig;
-import com.test.mydialog.R;
 
 /**
  * Created by Administrator on 2018/5/24.
@@ -26,18 +26,6 @@ import com.test.mydialog.R;
 
 public class LinearLayout4 extends LinearLayout {
     public boolean isDebug=true;
-    private double v;
-    private double v2;
-
-    private Region newRegion;
-    private Region leftRegion;
-    private Region topRegion;
-    private Region rightRegion;
-    private Region bottomRegion;
-    private int x;
-    private int y;
-    int touchFlag=-1;
-
     public void Log(String log) {
         if(BuildConfig.DEBUG&&isDebug){
             Log.i("ClockView===", log);
@@ -55,124 +43,132 @@ public class LinearLayout4 extends LinearLayout {
         super(context, attrs, defStyleAttr);
         init();
     }
-    Paint mPaint;
-    Paint pointPaint;
-    int strokeWidth=2;
+
     private void init() {
-        mPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
-//        mPaint.setAntiAlias(true);
-        mPaint.setDither(true);
-        mPaint.setStrokeWidth(strokeWidth);
-        mPaint.setStyle(Paint.Style.FILL);
-        mPaint.setStrokeJoin(Paint.Join.MITER);
-        mPaint.setStrokeCap(Paint.Cap.BUTT);
-        mPaint.setColor(ContextCompat.getColor(getContext(), R.color.blue));
-
-        pointPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
-        pointPaint.setDither(true);
-        pointPaint.setStrokeWidth(13);
-        pointPaint.setStyle(Paint.Style.FILL);
-        pointPaint.setColor(ContextCompat.getColor(getContext(), R.color.home_green));
-
+        borderColor= Color.TRANSPARENT;
+        borderColor=Color.BLUE;
     }
 
 
+    Paint mPaint;
+    Paint borderPaint;
+    float borderWidth=20;
+    int borderColor;
 
-    private Path leftPath;
-    private Path topPath;
-    private Path rightPath;
-    private Path bottomPath;
+    Path mPath;
+    Path borderPath;
+    Region clickRegion;
+    Region viewRegion;
+    boolean isCircle=true;
+
+    float allRadius=270;
+
+    float topLeftRadius;
+    float topRightRadius;
+    float bottomLeftRadius;
+    float bottomRightRadius;
+
+
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        RectF bRectF=new RectF(10,10,600,500);
-        RectF sRectF=new RectF(130,130,480,380);
-        RectF cRectF=new RectF(150,150,460,360);
-        int bAngle=84;
-        int sAngle=-82;
-        Region region=new Region (10,10,600,500);
-        newRegion = new Region();
-        leftRegion = new Region();
-        topRegion = new Region();
-        rightRegion = new Region();
-        bottomRegion = new Region();
+        clickRegion=new Region();
+        viewRegion=new Region(0,0,w,h);
+        mPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(Color.WHITE);
+        mPaint.setFilterBitmap(false);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
 
-        path = new Path();
-//        path.addCircle(300,300,300, Path.Direction.CW);
-        path.addArc(cRectF,0,360);
-        newRegion.setPath(path,region);
+        borderPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
+        borderPaint.setColor(borderColor);
+        borderPaint.setStyle(Paint.Style.STROKE);
+        borderPaint.setStrokeWidth(borderWidth);
+        float gradientLeft=getPaddingLeft()-borderWidth/2;
+        float gradientTop=getPaddingTop()-borderWidth/2;
+        float gradientRigth=w-getPaddingRight()+borderWidth/2;
+        float gradientBottom=h-getPaddingBottom()+borderWidth/2;
+        LinearGradient linearGradient = new LinearGradient(gradientLeft,
+                gradientTop,
+                gradientRigth,
+                gradientTop,
+                new int[]{Color.BLUE,Color.GREEN},
+                new float[]{0f,1f}, Shader.TileMode.CLAMP);
 
-        leftPath = new Path();
-        leftPath.addArc(bRectF,-40,bAngle);
-        leftPath.arcTo(sRectF,bAngle-40,sAngle);
-        leftPath.close();
-        leftRegion.setPath(leftPath,region);
+        int  X=(w-getPaddingLeft()-getPaddingRight())/2+getPaddingLeft();
+        int  Y=(h-getPaddingTop()-getPaddingBottom())/2+getPaddingTop();
+//        SweepGradient sweepGradient=new SweepGradient(X,Y,new int[]{Color.BLUE,Color.GREEN},new float[]{0.7f,1f});
+//        SweepGradient sweepGradient=new SweepGradient(X,Y,Color.BLUE,Color.GREEN);
+        RadialGradient radialGradient=new RadialGradient(X,Y,(w-getPaddingLeft()-getPaddingRight())/2,Color.BLUE,Color.GREEN, Shader.TileMode.CLAMP);
+//        borderPaint.setShader(radialGradient);
 
-        topPath = new Path();
-        topPath.addArc(bRectF,-130,bAngle);
-        topPath.arcTo(sRectF,bAngle-130,sAngle);
-        topPath.close();
-        topRegion.setPath(topPath,region);
+//        borderPaint.setPathEffect(new DashPathEffect(new float[]{22,22},1));
 
+        mPath=new Path();
+        borderPath=new Path();
+        float[]radius=new float[8];
+        if(allRadius>0){
+            radius[0]=allRadius;
+            radius[1]=allRadius;
 
+            radius[2]=allRadius;
+            radius[3]=allRadius;
 
-        rightPath = new Path();
-        rightPath.addArc(bRectF,-220,bAngle);
-        rightPath.arcTo(sRectF,bAngle-220,sAngle);
-        rightPath.close();
-        rightRegion.setPath(rightPath,region);
+            radius[4]=allRadius;
+            radius[5]=allRadius;
 
+            radius[6]=allRadius;
+            radius[7]=allRadius;
+        }else{
+            radius[0]=topLeftRadius;
+            radius[1]=topLeftRadius;
 
-        bottomPath = new Path();
-        bottomPath.addArc(bRectF,50,bAngle);
-        bottomPath.arcTo(sRectF,bAngle+50,sAngle);
-        bottomPath.close();
-        bottomRegion.setPath(bottomPath,region);
+            radius[2]=topRightRadius;
+            radius[3]=topRightRadius;
 
-        paint=new Paint(Paint.ANTI_ALIAS_FLAG);
-//        paint.setAntiAlias(true);
-        paint.setColor(ContextCompat.getColor(getContext(),R.color.theme_color));
-        paint.setStrokeWidth(10);
-        paint.setStyle(Paint.Style.FILL);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+            radius[4]=bottomLeftRadius;
+            radius[5]=bottomLeftRadius;
 
+            radius[6]=bottomRightRadius;
+            radius[7]=bottomRightRadius;
+        }
+        RectF rectF=new RectF(getPaddingLeft(),getPaddingTop(),w-getPaddingRight(),h-getPaddingBottom());
+        if(isCircle){//裁剪是以padding外部为界限还是padding内部
+            int  centerX=(w-getPaddingLeft()-getPaddingRight())/2+getPaddingLeft();
+            int  centerY=(h-getPaddingTop()-getPaddingBottom())/2+getPaddingTop();
 
+            int  circleRadius=(w-getPaddingLeft()-getPaddingRight())>(h-getPaddingTop()-getPaddingBottom())?(h-getPaddingTop()-getPaddingBottom())/2:(w-getPaddingLeft()-getPaddingRight())/2;
+            mPath.addCircle(centerX,centerY,circleRadius, Path.Direction.CW);
+            borderPath.addCircle(centerX,centerY,circleRadius, Path.Direction.CW);
+        }else{
+            mPath.addRoundRect(rectF,radius, Path.Direction.CW);
+            borderPath.addRoundRect(rectF,radius, Path.Direction.CW);
+        }
 
-        path=new Path();
-        RectF rectF=new RectF(getPaddingLeft(),getPaddingTop(),getWidth()-getPaddingRight(),getHeight()-getPaddingBottom());
-        path.addRoundRect(rectF,w/2,w/2, Path.Direction.CW);
-//        PointF pointF=new PointF(getWidth()/2,getHeight()/2);
-//        path.addCircle(pointF.x,pointF.y,w/2, Path.Direction.CW);
-//        path.moveTo(0, 0);  // 通过空操作让Path区域占满画布
-//        path.moveTo(w, h);
-    }
-
-
-
-    Paint paint;
-    Path path;
-    @SuppressLint("DrawAllocation")
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
+        clickRegion.setPath(mPath,viewRegion);
+        mPath.moveTo(0,0);
+        mPath.moveTo(w,h);
     }
 
     @Override
     protected void dispatchDraw(Canvas canvas) {
-//        int saveLayer = canvas.saveLayer(0, 0, getWidth(), getHeight(), null, Canvas.ALL_SAVE_FLAG);
+        int saveLayer = canvas.saveLayer(new RectF(0, 0, canvas.getWidth(), canvas.getHeight()), null, Canvas.ALL_SAVE_FLAG);
         super.dispatchDraw(canvas);
 
-//        canvas.drawPath(path,paint);
-//        paint.setXfermode(null);
-//        canvas.restoreToCount(saveLayer);
+        canvas.drawPath(mPath,mPaint);
+        canvas.drawPath(borderPath,borderPaint);
+        canvas.restoreToCount(saveLayer);
     }
 
     @Override
-    protected boolean drawChild(Canvas canvas, View child, long drawingTime) {
-        canvas.setDrawFilter(new PaintFlagsDrawFilter(0, Paint.FILTER_BITMAP_FLAG|Paint.ANTI_ALIAS_FLAG));
-        canvas.clipPath(path);
-
-        return super.drawChild(canvas, child, drawingTime);
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction()==MotionEvent.ACTION_UP){
+            if(clickRegion.contains((int)event.getX(),(int)event.getY())){
+                return super.onTouchEvent(event);
+            }else{
+                return false;
+            }
+        }
+        return super.onTouchEvent(event);
     }
 }
