645d0dc112f2bc25cda4196a486a82c05dd2a1af
zhongrui <zhongrui@021.com>
2020-08-17 10:45:52 +0800

    activity如果销毁，则不弹dialog

--- SimpleDialog/src/main/java/com/github/mydialog/TheDialog.java
+++ SimpleDialog/src/main/java/com/github/mydialog/TheDialog.java
@@ -1,17 +1,22 @@
 package com.github.mydialog;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatDialog;
 import android.view.Gravity;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class TheDialog extends AppCompatDialog  {
+public class TheDialog extends AppCompatDialog {
 
 
     private int width = WindowManager.LayoutParams.WRAP_CONTENT; //(int) (getScreenWidth() * 0.7);
@@ -21,14 +26,14 @@ public class TheDialog extends AppCompatDialog  {
     //    private int titleViewHeight = -1;
 //    private int titleViewColor = -1;
     private boolean translucentStatus = false;
-    private int backgroundColor=Color.WHITE;
-    private int backgroundDrawableResId =-1;
+    private int backgroundColor = Color.WHITE;
+    private int backgroundDrawableResId = -1;
     private Drawable backgroundDrawable;
-    private int paddingLeft=-1;
-    private int paddingTop=-1;
-    private int paddingRight=-1;
-    private int paddingBottom=-1;
-    private int gravity=Gravity.CENTER;
+    private int paddingLeft = -1;
+    private int paddingTop = -1;
+    private int paddingRight = -1;
+    private int paddingBottom = -1;
+    private int gravity = Gravity.CENTER;
     private float dimAmount = 0.3f;
     private float alpha = 1;
     private int leftRadius;
@@ -36,7 +41,7 @@ public class TheDialog extends AppCompatDialog  {
     private int rightRadius;
     private int bottomRadius;
 
-    private int animationId=-1;
+    private int animationId = -1;
 
 
     private Window window;
@@ -57,34 +62,40 @@ public class TheDialog extends AppCompatDialog  {
         super(context, cancelable, cancelListener);
         initWindow();
     }
+
     private void initWindow() {
-        window=getWindow();
+        window = getWindow();
 
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setDrawable();
     }
+
     public TheDialog setWidth(int width) {
-        this.width=width;
+        this.width = width;
         return this;
     }
+
     public TheDialog setHeight(int height) {
-        this.height=height;
+        this.height = height;
         return this;
     }
+
     public TheDialog setFullWidth() {
-        this.width=WindowManager.LayoutParams.MATCH_PARENT;
+        this.width = WindowManager.LayoutParams.MATCH_PARENT;
         return this;
     }
+
     public TheDialog setFullHeight() {
-        this.height=WindowManager.LayoutParams.MATCH_PARENT;
+        this.height = WindowManager.LayoutParams.MATCH_PARENT;
         return this;
     }
+
     public TheDialog setRadius(int radius) {
-        this.leftRadius=radius;
-        this.topRadius=radius;
-        this.rightRadius=radius;
-        this.bottomRadius=radius;
+        this.leftRadius = radius;
+        this.topRadius = radius;
+        this.rightRadius = radius;
+        this.bottomRadius = radius;
         setDrawable();
         return this;
     }
@@ -99,22 +110,25 @@ public class TheDialog extends AppCompatDialog  {
                 rightRadius, rightRadius,
                 bottomRadius, bottomRadius};
         gradientDrawable.setCornerRadii(fourRadius);
-        backgroundDrawable=gradientDrawable;
+        backgroundDrawable = gradientDrawable;
         window.setBackgroundDrawable(gradientDrawable);
     }
+
     public TheDialog setRadius(int leftRadius, int topRadius, int rightRadius, int bottomRadius) {
-        this.leftRadius=leftRadius;
-        this.topRadius=topRadius;
-        this.rightRadius=rightRadius;
-        this.bottomRadius=bottomRadius;
+        this.leftRadius = leftRadius;
+        this.topRadius = topRadius;
+        this.rightRadius = rightRadius;
+        this.bottomRadius = bottomRadius;
         setDrawable();
         return this;
     }
+
     public float getDimAmount() {
         return dimAmount;
     }
+
     public TheDialog setDimAmount(float dimAmount) {
-        this.dimAmount=dimAmount;
+        this.dimAmount = dimAmount;
         window.setDimAmount(this.dimAmount);
         return this;
     }
@@ -124,7 +138,7 @@ public class TheDialog extends AppCompatDialog  {
     }
 
     public TheDialog setAlpha(float alpha) {
-        this.alpha=alpha;
+        this.alpha = alpha;
         window.getAttributes().alpha = this.alpha;
         return this;
     }
@@ -134,7 +148,7 @@ public class TheDialog extends AppCompatDialog  {
     }
 
     public TheDialog setBackgroundDrawable(Drawable backgroundDrawable) {
-        this.backgroundDrawable=backgroundDrawable;
+        this.backgroundDrawable = backgroundDrawable;
         window.setBackgroundDrawable(backgroundDrawable);
         return this;
     }
@@ -144,7 +158,7 @@ public class TheDialog extends AppCompatDialog  {
     }
 
     public TheDialog setBackgroundColor(int backgroundColor) {
-        this.backgroundColor=backgroundColor;
+        this.backgroundColor = backgroundColor;
         setDrawable();
         return this;
     }
@@ -154,7 +168,7 @@ public class TheDialog extends AppCompatDialog  {
     }
 
     public TheDialog setBackgroundDrawableResource(int resId) {
-        this.backgroundDrawableResId=resId;
+        this.backgroundDrawableResId = resId;
         window.setBackgroundDrawableResource(this.backgroundDrawableResId);
         return this;
     }
@@ -164,7 +178,7 @@ public class TheDialog extends AppCompatDialog  {
     }
 
     public TheDialog setTranslucentStatus(boolean translucentStatus) {
-        this.translucentStatus= translucentStatus;
+        this.translucentStatus = translucentStatus;
         if (this.translucentStatus) {
             window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
@@ -172,11 +186,11 @@ public class TheDialog extends AppCompatDialog  {
     }
 
     public TheDialog setPadding(int paddingLeft, int paddingTop, int paddingRight, int paddingBottom) {
-        this.paddingLeft=paddingLeft;
-        this.paddingTop=paddingTop;
-        this.paddingRight=paddingRight;
-        this.paddingBottom=paddingBottom;
-        if(paddingLeft!=-1&&paddingTop!=-1&&paddingRight!=-1&&paddingBottom!=-1){
+        this.paddingLeft = paddingLeft;
+        this.paddingTop = paddingTop;
+        this.paddingRight = paddingRight;
+        this.paddingBottom = paddingBottom;
+        if (paddingLeft != -1 && paddingTop != -1 && paddingRight != -1 && paddingBottom != -1) {
             window.getDecorView().setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom);
         }
         return this;
@@ -184,7 +198,7 @@ public class TheDialog extends AppCompatDialog  {
 
 
     public TheDialog setPadding(int padding) {
-        return setPadding(padding,padding,padding,padding);
+        return setPadding(padding, padding, padding, padding);
     }
 
     public int getGravity() {
@@ -192,9 +206,9 @@ public class TheDialog extends AppCompatDialog  {
     }
 
     public TheDialog setGravity(int gravity) {
-        this.gravity=gravity;
+        this.gravity = gravity;
         window.setGravity(gravity);
-        if(animationId==-1){
+        if (animationId == -1) {
             setAnimation(-1);
         }
         return this;
@@ -205,7 +219,7 @@ public class TheDialog extends AppCompatDialog  {
     }
 
     public void setAnimationId(int animationId) {
-        if(this.animationId==animationId){
+        if (this.animationId == animationId) {
             return;
         }
         this.animationId = animationId;
@@ -213,8 +227,8 @@ public class TheDialog extends AppCompatDialog  {
     }
 
     public TheDialog setAnimation(int resId) {
-        if(resId==-1){
-            switch (gravity){
+        if (resId == -1) {
+            switch (gravity) {
                 case Gravity.TOP:
                     window.setWindowAnimations(R.style.MyDialogAnimationTop);
                     break;
@@ -222,8 +236,8 @@ public class TheDialog extends AppCompatDialog  {
                     window.setWindowAnimations(R.style.MyDialogAnimationBottom);
                     break;
             }
-        }else{
-            this.animationId=resId;
+        } else {
+            this.animationId = resId;
             window.setWindowAnimations(animationId);
         }
         return this;
@@ -231,11 +245,15 @@ public class TheDialog extends AppCompatDialog  {
 
     @Override
     public void show() {
+        if (isDestroyed(getContext())) {
+            return;
+        }
+
         WindowManager.LayoutParams lp = window.getAttributes();
-        if(this.width!=WindowManager.LayoutParams.WRAP_CONTENT){
+        if (this.width != WindowManager.LayoutParams.WRAP_CONTENT) {
             lp.width = width;
         }
-        if(this.height!=WindowManager.LayoutParams.WRAP_CONTENT){
+        if (this.height != WindowManager.LayoutParams.WRAP_CONTENT) {
             lp.height = height;
         }
         if (width != WindowManager.LayoutParams.WRAP_CONTENT || height != WindowManager.LayoutParams.WRAP_CONTENT) {
@@ -243,7 +261,39 @@ public class TheDialog extends AppCompatDialog  {
         }
 
 
-
         super.show();
+    }
+
+    public static Activity findActivity(Context context) {
+        if (context == null) {
+            return null;
+        } else {
+            if (context instanceof Activity) {
+                return (Activity) context;
+            } else if (context instanceof ContextWrapper) {
+                ContextWrapper wrapper = (ContextWrapper) context;
+                return findActivity(wrapper.getBaseContext());
+            } else {
+                return null;
+            }
+        }
+    }
+
+    public static boolean isDestroyed(Context context) {
+        Activity activity = findActivity(context);
+        if (activity == null) {
+            return true;
+        } else {
+            if (activity.isFinishing() || (Build.VERSION.SDK_INT >= 17 && activity.isDestroyed())) {
+                return true;
+            }
+            if (activity instanceof FragmentActivity) {
+                FragmentManager supportFragmentManager = ((FragmentActivity) activity).getSupportFragmentManager();
+                if (supportFragmentManager != null && supportFragmentManager.isDestroyed()) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
